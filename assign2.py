0f30b08578e568b458f2d1845b53ece7b94063a3
jonathank98 <regiskamgue@gmail.com>
2023-06-15 00:24:57 -0500

    assign2.py
    made changes to assign2.py file

--- assign2.py
+++ assign2.py
@@ -5,15 +5,14 @@ compname = input("Please enter your company's name: ")  #COMPANY NAME
 fiberprice = input("Please enter the length of fiber cable you will need: ") #GETTING FEET LENGTH FROM CUSTOMER
 fiberprice = int(fiberprice) #Converting feet length from string to inte
 cost = fiberprice * 0.87
-totalcost = cost + (0.0825 * cost)
+totalcost = cost
 print(fiberprice)
 
 print ("Here is your receipt!")
 
 print (compname)
 print ("The number of cable feet to be installed is: ", fiberprice)
-print ("The cost of installation is: $", cost)
-print ("The total cost of installation, with taxes included (8.25%) is: $ ", totalcost)
+print ("The total cost of installation is: $ ", totalcost)
 
 
 
